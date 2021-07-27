a8d8c6b887e65518a9cdd0ddfca9ddde8c80fa89
GK <it@soleriant.eu>
2021-07-27 09:00:37 +0100

    Initial Commit

---
+++ RandomSeries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomSeries
+//
+//  Created by WESTOWN on 27/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
